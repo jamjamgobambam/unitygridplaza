8b3d808e65150df797bd641e14e3f1fb95e9f131
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-03-26 13:07:55 -0400

    remove unnecessary components

--- client/src/components/FilteredEvents.jsx
+++ client/src/components/FilteredEvents.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Event from './Event'
 import '../css/Event.css'
 import '../css/FilteredEvents.css'
--- client/src/pages/Locations.jsx
+++ client/src/pages/Locations.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import LocationsAPI from '../services/LocationsAPI'
 import unitygrid from '../assets/unitygrid.jpg'
 import '../css/Locations.css'
